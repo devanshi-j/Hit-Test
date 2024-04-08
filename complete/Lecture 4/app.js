e4bc38d8eb7160957ec65a24ccb0bffa4784b944
devanshi-j <89416589+devanshi-j@users.noreply.github.com>
2024-04-08 08:10:12 +0530

    Update app.js

--- complete/Lecture 4/app.js
+++ complete/Lecture 4/app.js
@@ -66,7 +66,7 @@ class App{
     resize(){ 
         this.camera.aspect = window.innerWidth / window.innerHeight;
     	this.camera.updateProjectionMatrix();
-    	this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
     }
     
     loadKnight(){
